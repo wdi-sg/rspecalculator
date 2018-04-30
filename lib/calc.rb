8cdd5b389ebe60f9497e4863f1149cde88f66627
jchuajy <j.chua.jy@gmail.com>
2018-04-30 14:46:25 +0800

    done

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,2 +1,37 @@
 class Calculator
+      def add (a, b)
+
+            a + b
+
+      end
+
+      def subtract (a, b)
+
+            a - b 
+
+      end
+
+      def multiply (a, b)
+
+            a * b  
+
+      end
+
+      def divide (a, b)
+
+            a / b    
+
+      end
+
+      def power (a, b)
+
+            a ** b    
+            
+      end
+
+      def factorial (a)
+
+            (1..a).inject(1, :*) 
+            
+      end
 end
--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -14,6 +14,31 @@ describe Calculator do
 
   describe '#subtract' do
     it 'subtracts two positive numbers' do
+      expect(Calculator.new.subtract(3, 1)).to eq(2)
+    end
+  end
+
+  describe '#multiply' do
+    it 'multiplies two positive numbers' do
+      expect(Calculator.new.multiply(3, 4)).to eq(12)
+    end
+  end
+
+  describe '#divide' do
+    it 'divides two positive numbers' do
+      expect(Calculator.new.divide(4, 2)).to eq(2)
+    end
+  end
+
+  describe '#power' do
+    it 'a to the power of b' do
+      expect(Calculator.new.power(3, 5)).to eq(243)
+    end
+  end
+
+  describe '#factorial' do
+    it 'factorial of the number' do
+      expect(Calculator.new.factorial(5)).to eq(120)
     end
   end
 end
